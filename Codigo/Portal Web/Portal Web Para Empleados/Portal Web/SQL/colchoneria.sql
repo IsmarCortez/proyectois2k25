74533cdd4418bf8cd8820162b0e5f264a2b772ae
Javier-cyber-Wx <francisco12102002cl@gmail.com>
2025-05-20 01:10:12 -0600

    subiendo script de la bd y se modifico el index

--- Codigo/Portal Web/Portal Web Para Empleados/Portal Web/SQL/colchoneria.sql
+++ Codigo/Portal Web/Portal Web Para Empleados/Portal Web/SQL/colchoneria.sql
@@ -1,9 +1,3 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
---
--- Host: localhost    Database: colchoneria
--- ------------------------------------------------------
--- Server version	8.0.41
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
